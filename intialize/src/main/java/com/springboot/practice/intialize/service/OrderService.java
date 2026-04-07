7e20a9c396a54054c6c5503a2db98fcbf3ff28e0
Saikat <saikatsantra.16@gmail.com>
2026-04-07 09:53:54 +0530

    used setter injection to resolve circular dependency

--- intialize/src/main/java/com/springboot/practice/intialize/service/OrderService.java
+++ intialize/src/main/java/com/springboot/practice/intialize/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.springboot.practice.intialize.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,7 +9,12 @@ public class OrderService {
 	
 	private UserService user;
 	
-	public OrderService (UserService user) {
+//	public OrderService (@Lazy UserService user) {
+//		this.user = user;
+//	}
+	
+	@Autowired
+	public void setUserService(@Lazy UserService user) {
 		this.user = user;
 	}
 }
--- intialize/src/main/java/com/springboot/practice/intialize/service/UserService.java
+++ intialize/src/main/java/com/springboot/practice/intialize/service/UserService.java
@@ -1,5 +1,7 @@
 package com.springboot.practice.intialize.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,7 +9,12 @@ public class UserService {
 	
 	private OrderService order;
 	
-	public UserService(OrderService order) {
+//	public UserService(@Lazy OrderService order) {
+//		this.order = order;
+//	}
+	
+	@Autowired
+	public void setOrderService(@Lazy OrderService order) {
 		this.order = order;
 	}
 }
